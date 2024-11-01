30f9b721bf87e425932a80bd738f60115c4e1f81
Pradeep Antil <hmohammediaji@gmail.com>
2024-11-01 08:36:48 +0100

    update residencies component

---
+++ src/component/Value/Value.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import 'react-accessible-accordion/dist/fancy-example.css';
+import './Value.css'
+const Value = () => {
+  return (
+    <section className="v-wrapper">
+        <div className="paddings innerWidth flexCenter v-container">
+            {/* left side */}
+            <div className="v-left">
+                <img src="./value.png" alt="" />
+            </div>
+
+            right side 
+        </div>
+    </section>
+  )
+}
+
+export default Value
