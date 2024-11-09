3b2fbe0e0c049e01c31c9c7eb1879140089050ee
Pradeep Antil <hmohammediaji@gmail.com>
2024-11-09 09:08:11 +0100

    add GetStarted Section

--- src/component/GetStarted/GetStarted.jsx
+++ src/component/GetStarted/GetStarted.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import './GetStarted.css';
 
 const GetStarted = () => {
-  return (
-    <section className="g-wrapper">
-        <div className="g-container">
-            <h1>Get Started With Homyz</h1>
+    return (
+        <section className="gs-wrapper">
+            <div className="paddings innerWidth  gs-container">
+                <div className="flexColCenter inner-container">
+                    <h1>Get Started With Homyz</h1>
+                    <p className='secondaryText'>Subscribe and found super attractive price quote from us. <br />
+                        Find your residence soon</p>
 
-            <p>Subscribe and found super attractive price quote from us </p>
-        </div>
-    </section>
-  )
+                    <button className='gs-btn'>Get Started</button>
+
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default GetStarted;
